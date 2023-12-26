120d9490f5837bcccb83d0601ec46819d40fc28b
UnknownGravy <abdullahbinnur89@gmail.com>
2023-12-26 20:44:25 +0600

    removed lsp-zero and added more keymaps

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -6,4 +6,19 @@ keymap.set("n", "<leader>vs", vim.cmd.vs)
 keymap.set("n", "<leader>hs", vim.cmd.hs )
 keymap.set("n", "<leader>bp", vim.cmd.bprevious )
 keymap.set("n", "<leader>np", vim.cmd.bnext)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+--ripped from the primeagen
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+
+
 
